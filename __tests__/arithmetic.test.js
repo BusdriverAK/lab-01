69fbbd3a855091b9c1a1c125107769e74a1fe6ad
Mike K <m.kermes89@gmail.com>
2018-10-03 20:46:36 -0700

    nan test complete

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,4 +1,17 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
+const faker = require('faker');
+
+test('makes sure inputs are valid number', () =>{
+  expect(arithmetic.nan('dog', 3)).toBe(null);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(arithmetic.add(1, 2)).toBe(3);
+});
+
+test('subtracts 3 - 2 to equal 1', () =>{
+  expect(arithmetic.subtract(3, 2)).toBe(1);
+});
 
