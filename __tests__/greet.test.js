9953bf5de2ccbadad1d5d0a52b985ff105f65dca
Mike K <m.kermes89@gmail.com>
2018-10-03 21:34:22 -0700

    greet module and tests complete

---
+++ __tests__/greet.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+const faker = require('faker');
+
+test('returns null if not a string', ()=>{
+  expect(greet.hello(4)).toBe(null);
+});
+
+test('concats hello with a string', ()=>{
+  expect(greet.hello('Mike')).toBe('hello Mike');
+});
